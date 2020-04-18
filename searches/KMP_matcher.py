e9f86a08e0ff2ab747338b4146c76ff5043c949c
Julia Matveeva <juamatveeva@gmail.com>
2020-04-18 23:13:20 +0300

    format fixes

--- searches/KMP_matcher.py
+++ searches/KMP_matcher.py
@@ -1,16 +1,16 @@
-def KMP_matcher(T,P):
+def KMP_matcher(T, P):
     n = len(T)
     m = len(P)
     pi = compute_prefix(P)
     q = 0
     for i in range(n):
-        while q>0 and P[q] != T[i]:
+        while q > 0 and P[q] != T[i]:
             q = pi[q]
         if P[q] == T[i]:
             q = q + 1
-        if q==m:
-            print("Substring located with shift ", i-m+1)
-            q = pi[q-1]
+        if q == m:
+            print("Substring located with shift ", i - m + 1)
+            q = pi[q - 1]
 
 
 def compute_prefix(P):
@@ -19,10 +19,10 @@ def compute_prefix(P):
     pi.append(0)
     k = 0
     for q in range(1, m):
-        k = pi[q-1]
-        while k>0 and P[k] != P[q]:
-            k = pi[k-1]
-        if P[k]==P[q]:
+        k = pi[q - 1]
+        while k > 0 and P[k] != P[q]:
+            k = pi[k - 1]
+        if P[k] == P[q]:
             k = k + 1
         pi.append(k)
     return pi
--- searches/finite_automaton_matcher.py
+++ searches/finite_automaton_matcher.py
@@ -3,10 +3,10 @@ def compute_transition_function(P):
     m = len(P)
     for q in range(m):
         for a in range(256):
-            k = min(m, q+1)
-            while P[:k] not in P[:q]+chr(a):
-                k=k-1
-            delta[(q,chr(a))] = k #должен быть от 0 до m-1
+            k = min(m, q + 1)
+            while P[:k] not in P[:q] + chr(a):
+                k = k - 1
+            delta[(q, chr(a))] = k  # должен быть от 0 до m-1
     return delta
 
 
@@ -19,5 +19,5 @@ def finite_automaton_matcher(T, P):
         q = delta[(q, T[i])]
         if q == m:
             q = 0
-            s = i-m+1
+            s = i - m + 1
             print("Substring located with shift ", s)
